1701ea86cbea15b4b92b3e2fad0b22fba526a1d7
pedhip <95312778+PedHip@users.noreply.github.com>
2024-11-13 18:48:35 -0300

    Update cadastro_produtos.php

--- view/cadastro_produtos.php
+++ view/cadastro_produtos.php
@@ -25,7 +25,7 @@ $usuarios = $usuario->listarUsuarios();
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="backend.css">
+    <link rel="stylesheet" href="../styles/backend.css">
     <title>Cadastro e Listagem de Produtos</title>
 </head>
 <body>
