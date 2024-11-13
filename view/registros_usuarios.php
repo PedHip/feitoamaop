86821911f59bd0816af02b726e430ba4e5ae3282
pedhip <95312778+PedHip@users.noreply.github.com>
2024-11-13 18:49:59 -0300

    Update registros_usuarios.php

--- view/registros_usuarios.php
+++ view/registros_usuarios.php
@@ -26,7 +26,7 @@ $usuarios = $usuario->listarUsuarios();
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="backend.css">
+    <link rel="stylesheet" href="../styles/backend.css">
     <title>Registros de Usuários</title>
 </head>
 
