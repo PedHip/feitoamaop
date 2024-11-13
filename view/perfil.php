fe1f478931cd592ef4339c3a119f9f8b6fff27e0
pedhip <95312778+PedHip@users.noreply.github.com>
2024-11-13 18:49:33 -0300

    Update perfil.php

--- view/perfil.php
+++ view/perfil.php
@@ -20,7 +20,7 @@ if (isset($_SESSION['nome'])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Perfil - Carrinho de Compras</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="perfil.css">
+	<link rel="stylesheet" href="../styles/perfil.css">
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
 
