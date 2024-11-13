dc6cad7e82266e093b480d9ebe14c90d4e428726
pedhip <95312778+PedHip@users.noreply.github.com>
2024-11-13 18:45:15 -0300

    Update index.php

--- view/index.php
+++ view/index.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['nome'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-	<link rel="stylesheet" href="index.css">
+	<link rel="stylesheet" href="../styles/index.css">
     
 </head>
 
